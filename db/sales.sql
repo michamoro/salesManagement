9650e204255b275eadd4a53891e74a2cb4809a9a
Moro <mimoro1176@hotmail.com>
2021-09-27 16:20:50 -0500

    table names to lowcase

--- db/sales.sql
+++ db/sales.sql
@@ -21,10 +21,10 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: Goals; Type: TABLE; Schema: public; Owner: postgres
+-- Name: goals; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Goals" (
+CREATE TABLE public."goals" (
     id bigint NOT NULL,
     month_number integer NOT NULL,
     year_number integer NOT NULL,
@@ -32,14 +32,14 @@ CREATE TABLE public."Goals" (
 );
 
 
-ALTER TABLE public."Goals" OWNER TO postgres;
+ALTER TABLE public."goals" OWNER TO postgres;
 
 --
--- Name: Goals_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: goals_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE public."Goals" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public."Goals_id_seq"
+ALTER TABLE public."goals" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."goals_id_seq"
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -49,10 +49,10 @@ ALTER TABLE public."Goals" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- Name: Sales; Type: TABLE; Schema: public; Owner: postgres
+-- Name: sales; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Sales" (
+CREATE TABLE public."sales" (
     id bigint NOT NULL,
     date date NOT NULL,
     amount money NOT NULL,
@@ -60,14 +60,14 @@ CREATE TABLE public."Sales" (
 );
 
 
-ALTER TABLE public."Sales" OWNER TO postgres;
+ALTER TABLE public."sales" OWNER TO postgres;
 
 --
--- Name: Sales_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: sales_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE public."Sales" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public."Sales_id_seq"
+ALTER TABLE public."sales" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."sales_id_seq"
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -77,23 +77,23 @@ ALTER TABLE public."Sales" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- Name: Sellers; Type: TABLE; Schema: public; Owner: postgres
+-- Name: sellers; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public."Sellers" (
+CREATE TABLE public."sellers" (
     id bigint NOT NULL,
     name character varying(20) NOT NULL
 );
 
 
-ALTER TABLE public."Sellers" OWNER TO postgres;
+ALTER TABLE public."sellers" OWNER TO postgres;
 
 --
--- Name: Sellers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: sellers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-ALTER TABLE public."Sellers" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public."Sellers_id_seq"
+ALTER TABLE public."sellers" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."sellers_id_seq"
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -103,80 +103,80 @@ ALTER TABLE public."Sellers" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- Data for Name: Goals; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: goals; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Goals" (id, month_number, year_number, month_goal) FROM stdin;
+COPY public."goals" (id, month_number, year_number, month_goal) FROM stdin;
 \.
 
 
 --
--- Data for Name: Sales; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: sales; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Sales" (id, date, amount, id_seller) FROM stdin;
+COPY public."sales" (id, date, amount, id_seller) FROM stdin;
 \.
 
 
 --
--- Data for Name: Sellers; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: sellers; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Sellers" (id, name) FROM stdin;
+COPY public."sellers" (id, name) FROM stdin;
 \.
 
 
 --
--- Name: Goals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: goals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public."Goals_id_seq"', 1, false);
-
-
---
--- Name: Sales_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public."Sales_id_seq"', 1, false);
+SELECT pg_catalog.setval('public."goals_id_seq"', 1, false);
 
 
 --
--- Name: Sellers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: sales_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public."Sellers_id_seq"', 1, false);
-
-
---
--- Name: Goals Goals_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."Goals"
-    ADD CONSTRAINT "Goals_pkey" PRIMARY KEY (id);
+SELECT pg_catalog.setval('public."sales_id_seq"', 1, false);
 
 
 --
--- Name: Sales Sales_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: sellers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Sales"
-    ADD CONSTRAINT "Sales_pkey" PRIMARY KEY (id);
-
-
---
--- Name: Sellers Sellers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public."Sellers"
-    ADD CONSTRAINT "Sellers_pkey" PRIMARY KEY (id);
+SELECT pg_catalog.setval('public."sellers_id_seq"', 1, false);
 
 
 --
--- Name: Sales SellersFK; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: goals goals_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public."Sales"
-    ADD CONSTRAINT "SellersFK" FOREIGN KEY (id_seller) REFERENCES public."Sellers"(id) NOT VALID;
+ALTER TABLE ONLY public."goals"
+    ADD CONSTRAINT "goals_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: sales sales_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."sales"
+    ADD CONSTRAINT "sales_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: sellers sellers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."sellers"
+    ADD CONSTRAINT "sellers_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: sales sellersFK; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."sales"
+    ADD CONSTRAINT "sellersFK" FOREIGN KEY (id_seller) REFERENCES public."sellers"(id) NOT VALID;
 
 
 --
